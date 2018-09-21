55a12dd474271a47d15f696c8229a1b7a15d14df
Devin Mounts <devinmounts@gmail.com>
2018-09-21 12:13:59 -0700

    add day columns to calendar.

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -7,13 +7,51 @@ function Calendar(){
     border: 'solid black .5px',
     display: 'inline-block',
     float: 'right',
-    height: 'calc(16rem - 50px)',
+    height: 'calc(60vh - 0px)',
     position: 'relative',
     overflow: 'hidden',
   }
+
+  let weekRow = {
+    width: '100%',
+    height: '100%'
+
+  }
+
+  let dayColumn = {
+    display: 'inline-block',
+    width: '14%',
+    position: 'relative',
+    height: '100%',
+    border: 'solid black .5px',
+    backgroundColor: 'yellow'
+
+  }
   return(
     <div style={divStyle}>
-    Calendar Works
+      <div style={weekRow} >
+        <div style={dayColumn}>
+          <span>Sunday</span>
+        </div>
+        <div style={dayColumn}>
+          <span>Monday</span>
+        </div>
+        <div style={dayColumn}>
+          <span>Tuesday</span>
+        </div>
+        <div style={dayColumn}>
+          <span>Wednesday</span>
+        </div>
+        <div style={dayColumn}>
+          <span>Thursday</span>
+        </div>
+        <div style={dayColumn}>
+          <span>Friday</span>
+        </div>
+        <div style={dayColumn}>
+          <span>Saturday</span>
+        </div>
+      </div>
     </div>
   );
 
